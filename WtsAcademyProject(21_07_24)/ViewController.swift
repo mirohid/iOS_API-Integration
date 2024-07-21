d58b934d846b39cc1d6546a7be11c7bdf1fdb5d0
mirohid <mirohid7860@gmail.com>
2024-07-21 08:38:59 +0530

    Initial Commit

---
+++ WtsAcademyProject(21_07_24)/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WtsAcademyProject(21_07_24)
+//
+//  Created by Mir Ohid Ali on 21/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
